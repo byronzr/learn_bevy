0bd2b39721cf025988f84d76e0602f5f89a59070
byron <byron@live.com>
2025-01-17 14:27:51 +0800

    doc: lesson2_1.rs

--- README.md
+++ README.md
@@ -1,25 +1,20 @@
 # learn_bevy
 一步步，迈向游戏国度
 
-- lesson 1 ECS 
-  > 一个终端显示的调度与基础运行构建模型
-  > 间隔 2 秒不断打印 greeting
-  - lesson1_1 ECS
-	> 更完整 ECS 用例
-	> 一个模拟游戏得分的终端用例
-	> 包括 SystemSet 对 Update 的细分
-  - lesson1_2 Timer
-	> timer 计时器的应用
-	> 一个文本进度报告终端,模拟: 
-	> 一个实体(PrintOnCompletionTimer) 
-	> 一个超时间 Countdown.main_timer 
-	> 一个节点时间 Countdown.percent_trigger
-  - lesson1_3 Time<Real> / Time<Virtual>
-	> 关于游戏速度控制的例子,
-	> 在同一环境中两个 Sprite 分别互不影响的应用 Time<Real> 与 Time<Virtual> 速度
-  - lesson1_4 Time / Update
-	> Time 与 app.Update 的关系实验
-	> 在控制台用短字符交互,描述一个 FixedUpdate / Update 还有各种 Time 的关系
-- lesson 2 Event / Observe / Propagate
-  > 事件 (Event) 的监视(observe)与传播(propagate)
-  > 模拟一个身着盔甲的哥布林,承受伤害的过程
+## lesson 1 ECS 
+
+> 一个终端显示的调度与基础运行构建模型,间隔 2 秒不断打印 greeting
+
+- lesson1_1 更完整 ECS 用例,一个模拟游戏得分的终端用例,包括 SystemSet 对 Update 的细分.
+- lesson1_2 计时器的应用,一个文本进度报告终端. 
+- lesson1_3 在同一环境中两个 Sprite 分别互不影响的应用 Time<Real> 与 Time<Virtual> 速度.
+- lesson1_4 在控制台用短字符交互,描述一个 FixedUpdate / Update 还有各种 Time 的关系
+  
+  
+## lesson 2 Event / Observe / Propagate
+
+> 事件 (Event) 的监视(observe)与传播(propagate),模拟一个身着盔甲的哥布林,承受伤害的过程
+
+- lesson2_1 每个 system 都有一个 Local<T> 局布变量
+
+
---
+++ examples/lesson2_1.rs
@@ -0,0 +1,47 @@
+//! Shows how to create systems that run every fixed timestep, rather than every tick.
+
+use bevy::prelude::*;
+
+fn main() {
+    std::env::set_var("NO_COLOR", "1");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // this system will run once every update (it should match your screen's refresh rate)
+        .add_systems(Update, frame_update)
+        // add our system to the fixed timestep schedule
+        .add_systems(FixedUpdate, fixed_update)
+        // configure our fixed timestep schedule to run twice a second
+        .insert_resource(Time::<Fixed>::from_seconds(0.5))
+        .run();
+}
+
+// 使用 system 关联的 Local 局部存储计算出来的 delta 和 delta_secs() 相差无几
+// Local<T> 是一种 system 独有的局部变量.与每个 system 关联,T where Default,
+// 会在第一次使用前被初始化
+fn frame_update(mut last_time: Local<f32>, time: Res<Time>) {
+    // Default `Time` is `Time<Virtual>` here
+    info!(
+        "time since last frame_update: {} / {:?}",
+        time.elapsed_secs() - *last_time,
+        time.delta_secs(),
+    );
+    *last_time = time.elapsed_secs();
+}
+
+fn fixed_update(mut last_time: Local<f32>, time: Res<Time>, fixed_time: Res<Time<Fixed>>) {
+    // Default `Time`is `Time<Fixed>` here
+    info!(
+        "time since last fixed_update: {}/{:?}\n",
+        time.elapsed_secs() - *last_time,
+        time.delta_secs(),
+    );
+
+    info!("fixed timestep: {}\n", time.delta_secs());
+    // If we want to see the overstep, we need to access `Time<Fixed>` specifically
+    // 这里输出的是溢出的 delta
+    info!(
+        "time accrued toward next fixed_update: {}\n",
+        fixed_time.overstep().as_secs_f32()
+    );
+    *last_time = time.elapsed_secs();
+}
