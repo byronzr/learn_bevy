bcde80a5828531e6e8ad2aaf7b983c59904a266e
byron <byron@live.com>
2025-03-02 10:14:44 +0800

    add:  improve detail

--- examples/ui/render_ui_to_texture.rs
+++ examples/ui/render_ui_to_texture.rs
@@ -29,6 +29,7 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut images: ResMut<Assets<Image>>,
+    asset_server: Res<AssetServer>,
 ) {
     let size = Extent3d {
         width: 512,
@@ -37,6 +38,7 @@ fn setup(
     };
 
     // This is the texture that will be rendered to.
+    // ! 定义一个目标容器 (bevy_image)
     let mut image = Image::new_fill(
         size,
         TextureDimension::D2,
@@ -44,16 +46,19 @@ fn setup(
         TextureFormat::Bgra8UnormSrgb,
         RenderAssetUsages::default(),
     );
+
     // You need to set these texture usage flags in order to use the image as a render target
+    // ! 成为一个渲染目标,需要设置这些标志
     image.texture_descriptor.usage =
         TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST | TextureUsages::RENDER_ATTACHMENT;
 
+    // ! 将 image 加入到 image 的资源库中 (Assets<Image>)
     let image_handle = images.add(image);
 
     // Light
     commands.spawn(DirectionalLight::default());
 
-    // ! 这里,得到一个离屏渲染的 camera
+    // ! 这里,得到一个离屏渲染的 camera,相当于一个投射转换器
     let texture_camera = commands
         .spawn((
             Camera2d,
@@ -85,6 +90,11 @@ fn setup(
             TargetCamera(texture_camera),
         ))
         .with_children(|parent| {
+            // ! 纹理中使用图片
+            parent.spawn((ImageNode::new(
+                asset_server.load("branding/bevy_logo_dark_big.png"),
+            ),));
+            // ! 纹理中使用文字
             parent.spawn((
                 Text::new("This is a cube"),
                 TextFont {
@@ -95,9 +105,6 @@ fn setup(
             ));
         });
 
-    let cube_size = 4.0;
-    let cube_handle = meshes.add(Cuboid::new(cube_size, cube_size, cube_size));
-
     // This material has the texture that has been rendered.
     // ! 将 image_handle 加入材质库
     // ! 至此完成了关于一个纹理的创建的全部过程
@@ -105,9 +112,12 @@ fn setup(
         base_color_texture: Some(image_handle),
         reflectance: 0.02,
         unlit: false,
-
         ..default()
     });
+
+    // ! 开始使用该纹理
+    let cube_size = 4.0;
+    let cube_handle = meshes.add(Cuboid::new(cube_size, cube_size, cube_size));
 
     // Cube with material containing the rendered UI texture.
     commands.spawn((
@@ -118,6 +128,7 @@ fn setup(
     ));
 
     // The main pass camera.
+    // ! 主视窗的 camera
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 0.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
