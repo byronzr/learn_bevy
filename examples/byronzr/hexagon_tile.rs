917f411659267fd3d2963d686e40b4f3da237a40
byron <byron@live.com>
2025-03-07 13:30:12 +0800

    save

--- examples/byronzr/hexagon_tile.rs
+++ examples/byronzr/hexagon_tile.rs
@@ -1,14 +1,13 @@
-use std::time::Duration;
+use std::fs;
 
 use bevy::{
     asset::LoadedFolder,
-    ecs::event,
     prelude::*,
     sprite::Anchor,
-    utils::HashMap,
-    winit::{UpdateMode, WinitSettings},
+    utils::{HashMap, HashSet},
 };
 use rand::{rng, seq::IndexedRandom};
+use serde::{Deserialize, Serialize};
 
 // 分层
 const TERRAIN_LAYER: f32 = 1.; // 地形
@@ -31,6 +30,21 @@ const HEXAGON_HALF_SIZE: f32 = 16.; // 一半
 const HEXAGON_GAP: f32 = 1.; // 间隔
 const HEXAGON_SIDE_LENGTH: f32 = 18.; // 边长
 const HEXAGON_SIDE_WIDTH: f32 = 7.; // 侧边宽度
+
+// 迷雾逐圈数据
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct FowRange {
+    // 奇偶行不一样
+    // [奇偶行][等级][坐标]
+    pub range: Vec<Vec<Vec<(i32, i32)>>>,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct FowSet {
+    // 奇偶行不一样
+    // [奇偶行][等级][坐标]
+    pub range: Vec<Vec<HashSet<(i32, i32)>>>,
+}
 
 // 可视元素基本信息(AtlasInfo)
 #[derive(Component, Debug, Clone, Default)]
@@ -95,17 +109,20 @@ struct PlayerInfo {
     pub movement_range: usize,     // 移动距离
     pub sight_range: usize,        // 视野距离
     pub destination: Vec2,         // 目标
+    pub flipping: bool,            // 翻转(记录着最后一次移动方向)
 }
 
-// FOW 等级
+// FOW 坐标
 #[derive(Debug, Component)]
 struct FowCoor(usize, usize);
 
-#[derive(Debug, Default, Component)]
-struct TerrainMarker;
+// FOW 等级
+#[derive(Debug, Component)]
+struct FowLevel(usize);
 
+// 地形标记
 #[derive(Debug, Default, Component)]
-struct CoorRange(usize);
+struct TerrainMarker(usize, usize);
 
 // 每个 Tile 的具体信息
 #[derive(Debug, Default)]
@@ -118,10 +135,14 @@ struct TileMap {
     pub player: Option<ElementInfo>,
     pub description: Option<String>,
 }
+
 // 世界大地图(每个tile)
 #[derive(Resource, Debug, Default)]
 struct WorldMap {
     pub map: HashMap<(usize, usize), TileMap>,
+    pub destination_coordiate: Option<(usize, usize)>, // 玩家移动目标,缓存至移动完毕
+    pub reachable_coordiate_set: HashSet<(usize, usize)>, // 可达坐标
+    pub fow_range: Option<FowSet>,                     // 雾逐圈数据
 }
 
 fn main() {
@@ -130,14 +151,11 @@ fn main() {
     app.init_state::<GameState>();
     app.init_resource::<PretreatSet>();
     app.init_resource::<WorldMap>();
-    app.insert_resource(WinitSettings {
-        focused_mode: UpdateMode::reactive_low_power(Duration::from_millis(100)),
-        ..default()
-    });
+
     // init player info
     let inf = PlayerInfo {
         coordiate: (7, 14),
-        movement_range: 1,
+        movement_range: 2,
         sight_range: 3,
         ..Default::default()
     };
@@ -148,93 +166,100 @@ fn main() {
         (
             check_textures.after(load_textures),
             clear_fow.after(render_map),
-            mouse_over,
+            mouse_action,
         ),
     );
-    app.add_systems(FixedPostUpdate, animate_player.after(render_map));
+    app.add_systems(
+        RunFixedMainLoop,
+        animate_player.in_set(RunFixedMainLoopSystem::AfterFixedMainLoop),
+    );
     app.add_systems(OnEnter(GameState::GenerateWorld), generate_map_data);
     app.add_systems(OnEnter(GameState::InGame), render_map);
     app.run();
 }
 
+// 清理迷雾
 fn clear_fow(
-    mut commands: Commands,
-    mut query: Query<(Entity, &mut Sprite, &FowCoor)>,
+    mut query: Query<(&mut Sprite, &FowCoor, &mut FowLevel)>,
     player_info: Res<PlayerInfo>,
+    mut world_map: ResMut<WorldMap>,
 ) {
-    let mut range = vec![];
-    range.push(vec![(0, 0)]);
-    range.push(
-        // level 1
-        vec![(0, 0), (0, -2), (0, -1), (0, 1), (0, 2), (-1, 1), (-1, -1)],
-    );
-    range.push(
-        // level 2
-        vec![
-            (0, -4),
-            (0, -3),
-            (1, -2),
-            (1, 0),
-            (1, 2),
-            (0, 3),
-            (0, 4),
-            (-1, 3),
-            (-1, 2),
-            (-1, 0),
-            (-1, -2),
-            (-1, -3),
-        ],
-    );
+    // 初始化 fow_range
+    if world_map.fow_range.is_none() {
+        let fow_path = "./assets/storage/fow.json";
+        let Ok(content) = fs::read_to_string(fow_path) else {
+            error!("can't read fow.json");
+            return;
+        };
 
-    range.push(
-        // level 3
-        vec![
-            (0, -6),
-            (0, -5),
-            (1, -4),
-            (1, -3),
-            (1, -1),
-            (1, 1),
-            (1, 3),
-            (1, 4),
-            (0, 5),
-            (0, 6),
-            (-1, 5),
-            (-1, 4),
-            (-2, 3),
-            (-2, 1),
-            (-2, -1),
-            (-2, -3),
-            (-1, -4),
-            (-1, -5),
-        ],
-    );
+        let Ok(fow) = serde_json::from_str::<FowSet>(&content) else {
+            error!("can't parse fow.json");
+            return;
+        };
+        world_map.fow_range = Some(fow);
+    }
 
-    for (entity, mut sprite, coor) in query.iter_mut() {
+    let fow = world_map.fow_range.as_ref().unwrap();
+
+    // let mut fow_set = FowSet::default();
+    // for range in fow.range.iter() {
+    //     let mut row = vec![];
+
+    //     for iter in range.iter() {
+    //         let mut set = HashSet::new();
+    //         for v in iter {
+    //             set.insert(*v);
+    //         }
+    //         row.push(set);
+    //     }
+    //     fow_set.range.push(row);
+    // }
+
+    // write to file fow_set.json
+    // let fow_set_path = "./assets/storage/fow_set.json";
+    // let Ok(content) = serde_json::to_string(&fow_set) else {
+    //     error!("can't serialize fow_set.json");
+    //     return;
+    // };
+    // let Ok(_) = fs::write(fow_set_path, content) else {
+    //     error!("can't write fow_set.json");
+    //     return;
+    // };
+
+    let mut reachable_set = HashSet::new();
+    for (mut sprite, fow_coor, mut fow_level) in query.iter_mut() {
         let current = (
-            coor.0 as i32 - player_info.coordiate.0 as i32,
-            coor.1 as i32 - player_info.coordiate.1 as i32,
+            fow_coor.0 as i32 - player_info.coordiate.0 as i32,
+            fow_coor.1 as i32 - player_info.coordiate.1 as i32,
         );
+        // 按玩家所在奇偶行取不同的范围
+        let Some(range) = fow.range.get(player_info.coordiate.1 % 2) else {
+            continue;
+        };
         for (level, iter) in range.iter().enumerate() {
             for v in iter {
                 if current == *v {
-                    let alpha = level as f32 / player_info.sight_range as f32;
-                    if alpha < 0. {
-                        commands.entity(entity).despawn();
-                    } else {
+                    if level < fow_level.0 {
+                        fow_level.0 = level;
+                        let alpha = fow_level.0 as f32 / player_info.sight_range as f32;
                         sprite.color = Color::srgba(0., 0., 0., alpha);
                     }
-                    Color::srgba(0., 0., 0., level as f32 / player_info.sight_range as f32);
-                } else {
+                    // sprite.color = Color::srgba(0., 0., 0., 0.);
+                    if level > 0 && level <= player_info.movement_range {
+                        reachable_set.insert((fow_coor.0, fow_coor.1));
+                    }
                 }
             }
         }
     }
+    if world_map.reachable_coordiate_set != reachable_set {
+        world_map.reachable_coordiate_set = reachable_set;
+    }
 }
 
-// animate player
+// 玩家动画切换与移动
 fn animate_player(
-    time: Res<Time>,
+    time: Res<Time<Fixed>>,
     mut query: Query<(
         &mut PlayerTimer,
         &mut Sprite,
@@ -242,8 +267,9 @@ fn animate_player(
         &mut Transform,
         &mut PlayerState,
     )>,
-    player_info: Res<PlayerInfo>,
+    mut player_info: ResMut<PlayerInfo>,
     pretreat: Res<PretreatSet>,
+    mut world_map: ResMut<WorldMap>,
 ) {
     let Ok((mut timer, mut sprite, indices, mut transform, mut state)) = query.get_single_mut()
     else {
@@ -260,7 +286,6 @@ fn animate_player(
 
     if distance > 1. {
         // 找到目标方向,不需要转换
-        // TODO: 镜像
         let front = to_direction / distance;
         let step = speed * time.delta_secs() * front;
         transform.translation += step.extend(0.);
@@ -272,11 +297,7 @@ fn animate_player(
                 .unwrap()
                 .sprite
                 .clone();
-            if to_direction.x < 0. {
-                sprite.flip_x = true;
-            } else {
-                sprite.flip_x = false;
-            }
+            player_info.flipping = if to_direction.x < 0. { true } else { false }
         }
         let Some(atlas) = &mut sprite.texture_atlas else {
             return;
@@ -295,6 +316,9 @@ fn animate_player(
                 .sprite
                 .clone();
             transform.translation = player_info.destination.extend(PLAYER_LAYER);
+            if let Some(coor) = world_map.destination_coordiate.take() {
+                player_info.coordiate = coor;
+            }
         }
         let Some(atlas) = &mut sprite.texture_atlas else {
             return;
@@ -304,33 +328,37 @@ fn animate_player(
             atlas.index = indices.first;
         }
     }
+    sprite.flip_x = player_info.flipping;
 }
 
 /// 是否选中平顶六边形
-/// hex_radius 边长,不是半径
-pub fn point_in_flat_top_hexagon(point: Vec2, hex_center: Vec2, hex_radius: f32) -> bool {
+pub fn point_in_flat_top_hexagon(point: Vec2, hex_center: Vec2, hex_side_length: f32) -> bool {
     let q2x = f32::abs(point.x - hex_center.x);
     let q2y = f32::abs(point.y - hex_center.y);
-    let h = hex_radius * 0.866;
-
-    if q2x > hex_radius || q2y > h {
+    let h = hex_side_length * 0.866;
+    if q2x > hex_side_length || q2y > h {
         return false;
     }
-    if q2x <= hex_radius * 0.5 {
+    if q2x <= hex_side_length * 0.5 {
         return true;
     }
-
-    let q3x = h - (2. * h / hex_radius) * (q2x - hex_radius / 2.);
+    let q3x = h - (2. * h / hex_side_length) * (q2x - hex_side_length / 2.);
     return q2y <= q3x;
 }
 
-fn mouse_over(
+// 鼠标点击与移动事件
+fn mouse_action(
     mut events: EventReader<CursorMoved>,
     camera: Single<(&Camera, &GlobalTransform)>,
-    mut query: Query<(&mut Sprite, &Transform), With<TerrainMarker>>,
+    mut query: Query<(&mut Sprite, &Transform, &TerrainMarker)>,
     input: Res<ButtonInput<MouseButton>>,
     mut player_info: ResMut<PlayerInfo>,
+    mut world_map: ResMut<WorldMap>,
 ) {
+    // 还在移动中,不显示颜色,也不允许点击
+    if world_map.destination_coordiate.is_some() {
+        return;
+    }
     let Some(event) = events.read().last() else {
         return;
     };
@@ -338,16 +366,21 @@ fn mouse_over(
     let Ok(world_position) = camera.viewport_to_world_2d(camera_transform, event.position) else {
         return;
     };
-
-    for (mut terrain, transform) in &mut query {
+    for (mut terrain, transform, marker) in &mut query {
         if point_in_flat_top_hexagon(
             world_position,
             transform.translation.truncate(),
             HEXAGON_SIDE_LENGTH,
         ) {
-            terrain.color = Color::srgba(0., 1., 0., 0.5);
-            if input.just_pressed(MouseButton::Left) {
-                player_info.destination = transform.translation.truncate();
+            let coor = (marker.0, marker.1);
+            if world_map.reachable_coordiate_set.contains(&coor) {
+                terrain.color = Color::srgba(0., 1., 0., 0.5);
+                if input.just_pressed(MouseButton::Left) {
+                    player_info.destination = transform.translation.truncate();
+                    world_map.destination_coordiate = Some(coor);
+                }
+            } else {
+                terrain.color = Color::srgba(1., 0., 0., 0.5);
             }
         } else {
             terrain.color = Color::WHITE;
@@ -357,14 +390,14 @@ fn mouse_over(
 
 // 初始化地图
 fn render_map(
+    asset_server: Res<AssetServer>,
     world_map: Res<WorldMap>,
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
     pretreat: Res<PretreatSet>,
     mut player_info: ResMut<PlayerInfo>,
 ) {
     let mut start_position = Vec2::new(0., 0.);
-    let start_coordiante = (7, 14);
+    let start_coordiante = (8, 15);
     for col in 0..COLS {
         for row in 0..ROWS {
             let Some(tm) = world_map.map.get(&(col, row)) else {
@@ -385,7 +418,7 @@ fn render_map(
             commands.spawn((
                 ei.sprite,
                 Transform::from_translation(tm.position.extend(TERRAIN_LAYER)),
-                TerrainMarker,
+                TerrainMarker(col, row),
             ));
 
             // 建筑
@@ -411,24 +444,29 @@ fn render_map(
                 .spawn((
                     fow,
                     FowCoor(col, row),
+                    FowLevel(99),
                     Transform::from_translation(tm.position.extend(FOW_LAYER)),
                 ))
                 .with_children(|parent| {
                     // 坐标
-                    // parent.spawn((
-                    //     Text2d(format!(
-                    //         "{},{}",
-                    //         col as i32 - start_coordiante.0 as i32,
-                    //         row as i32 - start_coordiante.1 as i32
-                    //     )),
-                    //     TextFont {
-                    //         font: asset_server.load("fonts/SourceHanSansCN-Normal.otf"),
-                    //         font_size: 12.0,
-                    //         ..default()
-                    //     },
-                    //     //TextColor(Color::BLACK),
-                    //     Transform::from_translation(Vec3::new(0., 0., COORDIANTE_LAYER)),
-                    // ));
+                    parent.spawn((
+                        // 差值坐标
+                        Text2d(format!(
+                            "{},{}",
+                            col as i32 - start_coordiante.0 as i32,
+                            row as i32 - start_coordiante.1 as i32
+                        )),
+                        // 原始坐标
+                        //Text2d(format!("{},{}", col, row)),
+                        TextFont {
+                            font: asset_server.load("fonts/SourceHanSansCN-Normal.otf"),
+                            font_size: 12.0,
+                            ..default()
+                        },
+                        //TextColor(Color::BLACK),
+                        //Transform::from_translation(Vec3::new(0., 0., COORDIANTE_LAYER)),
+                        //Visibility::Hidden,
+                    ));
                 });
         }
     }
@@ -440,11 +478,12 @@ fn render_map(
         commands.spawn((
             idle.sprite.clone(),
             AnimationIndices { first: 0, last: 5 },
-            PlayerTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+            PlayerTimer(Timer::from_seconds(0.05, TimerMode::Repeating)),
             PlayerState::Idle,
             transform,
         ));
         player_info.destination = start_position;
+        player_info.coordiate = start_coordiante;
     }
 }
 
@@ -472,7 +511,6 @@ fn check_textures(
     asset_server: Res<AssetServer>,
 ) {
     for event in event_reader.read() {
-        info!("checking...");
         if event.is_loaded_with_dependencies(&texture.0) {
             // 归纳所有图片
             // tile 96 x 768 = 3 x 24
@@ -612,13 +650,13 @@ fn generate_map_data(
     next_state.set(GameState::InGame);
 }
 
+// 随机地图数据
 fn random_data(world_map: &mut WorldMap, pretreat: &PretreatSet) {
     let mut rng = rng();
     let center = pretreat.window_size / 2.;
     // 起始左上角 = 在上角坐标 + (地块一半+整体偏移量)
     let topleft = Vec2::new(-center.x, center.y)
         + (Vec2::splat(HEXAGON_HALF_SIZE) + MAP_OFFSET) * Vec2::new(1., -1.);
-    println!("center: {}", center);
     for col in 0..COLS {
         for row in 0..ROWS {
             // 随机地形
