48182eca2fda008591ad5267a8210227522493e2
byron <byron@live.com>
2025-08-17 03:50:31 +0800

    improve shader_game_of_life

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
 fn main() {
-    todo!()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+    let image = Image::default();
+
+    commands.spawn((
+        Sprite {
+            image: images.add(image),
+            ..default()
+        },
+        Transform::default(),
+    ));
+
+    commands.spawn(Camera2d);
 }
