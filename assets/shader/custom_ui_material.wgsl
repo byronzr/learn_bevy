cd68a189e4558e8e4e011b814345cf1cb3d66ec2
byron <byron@live.com>
2025-08-20 07:50:38 +0800

    add  UiMaterial

---
+++ assets/shader/custom_ui_material.wgsl
@@ -0,0 +1,103 @@
+// // The Vertex output of the default vertex shader for the Ui Material pipeline.
+// struct UiVertexOutput {
+//     @location(0) uv: vec2<f32>,
+//     ! The size of the borders in UV space. Order is Left, Right, Top, Bottom.
+//     @location(1) border_widths: vec4<f32>,
+//     ! The size of the borders in pixels. Order is top left, top right, bottom right, bottom left.
+//     @location(2) border_radius: vec4<f32>,
+//     ! The size of the node in pixels. Order is width, height.
+//     @location(3) @interpolate(flat) size: vec2<f32>,
+//     @builtin(position) position: vec4<f32>,
+// };
+
+
+// Draws a progress bar with properties defined in CustomUiMaterial
+#import bevy_ui::ui_vertex_output::UiVertexOutput
+
+// ! UI使用的group居然是在第1组
+@group(1) @binding(0) var<uniform> color: vec4<f32>;
+@group(1) @binding(1) var<uniform> slider: vec4<f32>;
+@group(1) @binding(2) var material_color_texture: texture_2d<f32>;
+@group(1) @binding(3) var material_color_sampler: sampler;
+@group(1) @binding(4) var<uniform> border_color: vec4<f32>;
+
+
+@fragment
+fn fragment(in: UiVertexOutput) -> @location(0) vec4<f32> {
+    let output_color = textureSample(material_color_texture, material_color_sampler, in.uv) * color;
+
+    // half size of the UI node
+    let half_size = 0.5 * in.size;
+
+    // position relative to the center of the UI node
+    // UV 是 NTC (Normalized Texture Coordinates) 坐标系
+    // ! 当前渲染像素相对于于 UI 节点中心的偏移(从NTC转换到像素坐标系)
+    let p = in.uv * in.size - half_size;
+
+    // ! 验证 select
+    // var b = vec2(0.0,0.0);
+    // if (0.<p.x) {
+    //     b.x = in.border_widths.z; 
+    // }else{
+    //     b.x = in.border_widths.x;
+    // }
+
+    // if (0. <p.y){
+    //     b.y = in.border_widths.w;
+    // }else{
+    //     b.y = in.border_widths.y;
+    // }
+    // thickness of the border closest to the current position
+    // p 在第一象限时,x值为正,y值为负(+x,-y),b值为 (z,y) => (上,右)
+    // p 在第二象限时,x值为负,y值为负(-x,-y),b值为 (x,y) => (左,上)
+    // p 在第三象限时,x值为负,y值为正(-x,+y),b值为 (x,w) => (左,下)
+    // p 在第四象限时,x值为正,y值为正(+x,+y),b值为 (z,w) => (上,下)
+    let b = vec2(
+        // ! cond ? b : a
+        // ! 如果 p.x 为负数,说明当前像素在左边,返回 x(LEFT),反之返回 z(TOP)
+        select(in.border_widths.x, in.border_widths.z, 0. < p.x),
+        // ! 如果 p.y 为负数,说明当前像素在上边,返回 y(RIGHT),反之返回 w(BOTTOM)
+        select(in.border_widths.y, in.border_widths.w, 0. < p.y)
+    );
+
+    // select radius for the nearest corner
+    // ! xy = (上左,上右) wz = (下左,下右)
+    let rs = select(in.border_radius.xy, in.border_radius.wz, 0.0 < p.y);
+    let radius = select(rs.x, rs.y, 0.0 < p.x);
+
+    // distance along each axis from the corner
+    // ! 计算当前像素到边框的距离
+    let d = half_size - abs(p);
+
+
+    // ! 绘制边框
+    // ! 如果当前像素到边框的距离小于边框宽度,则
+    // ! 说明当前像素在边框内,返回边框颜色(进入)
+    // if the distance to the edge from the current position on any axis 
+    // is less than the border width on that axis then the position is within 
+    // the border and we return the border color
+    if d.x < b.x || d.y < b.y {
+        // ! 如果当前像素到边框的距离小于圆角半径,则说明当前像素在圆角内
+        // determine if the point is inside the curved corner and return the corresponding color
+        let q = radius - d;
+        
+        if radius < min(max(q.x, q.y), 0.0) + length(vec2(max(q.x, 0.0), max(q.y, 0.0))) {
+            // ! 圆角外(透明)
+            return vec4(0.0);
+        } else {
+            // ! 圆角内(边框颜色)
+            return border_color;
+        }
+    }
+
+    // sample the texture at this position if it's to the left of the slider value
+    // otherwise return a fully transparent color
+    // ! 如果当前像素在滑块左边,则返回纹理颜色(正片叠底 multiply/调制 modulate)
+    // @ 其实接管 Node 绘制最主要的部份就在此,之前的都是绘制边框与圆角(可以硬抄)
+    if in.uv.x < slider.x {
+        let output_color = textureSample(material_color_texture, material_color_sampler, in.uv) * color;
+        return output_color;
+    } else {
+        return vec4(0.0);
+    }
+}
---
+++ examples/ui/ui_material.rs
@@ -0,0 +1,113 @@
+//! Demonstrates the use of [`UiMaterials`](UiMaterial) and how to change material values
+
+use bevy::{
+    color::palettes::css::DARK_BLUE, prelude::*, reflect::TypePath, render::render_resource::*,
+    shader::ShaderRef,
+};
+
+/// This example uses a shader source file from the assets subdirectory
+const SHADER_ASSET_PATH: &str = "shaders/custom_ui_material.wgsl";
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(UiMaterialPlugin::<CustomUiMaterial>::default())
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut ui_materials: ResMut<Assets<CustomUiMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // Camera so we can see UI
+    commands.spawn(Camera2d);
+
+    let boarder = UiRect::new(Val::Px(10.), Val::Px(20.), Val::Px(30.), Val::Px(40.));
+
+    commands
+        .spawn(Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        })
+        .with_children(|parent| {
+            let banner_scale_factor = 0.5;
+            parent.spawn((
+                Node {
+                    position_type: PositionType::Absolute,
+                    width: Val::Px(905.0 * banner_scale_factor), // ! 452
+                    height: Val::Px(363.0 * banner_scale_factor), // ! 181
+                    border: boarder,                             // ! 会传入 shader (boarder_widths)
+                    ..default()
+                },
+                // ! 一但使用 MaterialNode,边框与圆角就需要自行绘制(in shader)
+                // ! 452*181 是真正在 shader 中使用的 size(含boarder)
+                MaterialNode(ui_materials.add(CustomUiMaterial {
+                    color: LinearRgba::WHITE.to_f32_array().into(),
+                    slider: Vec4::splat(0.5),
+                    // ! size = 905x363
+                    color_texture: asset_server.load("branding/banner.png"),
+                    border_color: LinearRgba::WHITE.to_f32_array().into(),
+                })),
+                BorderRadius::all(Val::Px(10.)), // ! 会传入 shader (border_radius)
+                // UI material nodes can have outlines and shadows like any other UI node
+                Outline {
+                    width: Val::Px(2.),
+                    offset: Val::Px(100.),
+                    color: DARK_BLUE.into(),
+                },
+            ));
+        });
+}
+
+#[derive(AsBindGroup, Asset, TypePath, Debug, Clone)]
+struct CustomUiMaterial {
+    /// Color multiplied with the image
+    #[uniform(0)]
+    color: Vec4,
+    /// Represents how much of the image is visible
+    /// Goes from 0 to 1
+    /// A `Vec4` is used here because Bevy with webgl2 requires that uniforms are 16-byte aligned but only the first component is read.
+    #[uniform(1)]
+    slider: Vec4,
+    /// Image used to represent the slider
+    #[texture(2)]
+    #[sampler(3)]
+    color_texture: Handle<Image>,
+    /// Color of the image's border
+    #[uniform(4)]
+    border_color: Vec4,
+}
+
+impl UiMaterial for CustomUiMaterial {
+    fn fragment_shader() -> ShaderRef {
+        SHADER_ASSET_PATH.into()
+    }
+}
+
+// Fills the slider slowly over 2 seconds and resets it
+// Also updates the color of the image to a rainbow color
+fn animate(
+    mut materials: ResMut<Assets<CustomUiMaterial>>,
+    q: Query<&MaterialNode<CustomUiMaterial>>,
+    time: Res<Time>,
+) {
+    let duration = 2.0;
+    for handle in &q {
+        if let Some(material) = materials.get_mut(handle) {
+            // rainbow color effect
+            let new_color = Color::hsl((time.elapsed_secs() * 60.0) % 360.0, 1., 0.5);
+            let border_color = Color::hsl((time.elapsed_secs() * 60.0) % 360.0, 0.75, 0.75);
+            material.color = new_color.to_linear().to_vec4();
+            // material.slider.x =
+            //     ((time.elapsed_secs() % (duration * 2.0)) - duration).abs() / duration;
+            material.slider.x = time.elapsed_secs().sin().abs();
+            material.border_color = border_color.to_linear().to_vec4();
+        }
+    }
+}
